501813b060ad8cc7cb5acf34dbf0e1bb0cd08538
vadelgado <vadelgado@udenar.edu.co>
2024-02-22 14:01:36 -0500

    22/02/2024 Cmabios Layout Comprobantes @vadelgado

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -50,26 +50,22 @@ export default function Authenticated({ user, header, children }) {
                                 )}
                             </div>
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                {user.role === "acudiente" && (
-                                    <NavLink
-                                        href={route("comprobantes.index")}
-                                        activate={route().current(
-                                            "comprobantes.index"
-                                        )}
-                                    >
-                                        Comprobantes
-                                    </NavLink>
-                                )}
-                                {user.role === "admin" && (
-                                    <NavLink
-                                        href={route("comprobantes.indexAdmin")}
-                                        activate={route().current(
-                                            "comprobantes.indexAdmin"
-                                        )}
-                                    >
-                                        Comprobantes
-                                    </NavLink>
-                                )}
+                            {user.role === "acudiente" && (
+                                        <NavLink
+                                            href={route("comprobantes.index")}
+                                            activate={route().current("comprobantes.index") ? "true" : "false"}
+                                        >
+                                            Comprobantes
+                                        </NavLink>
+                                    )}
+                                    {user.role === "admin" && (
+                                        <NavLink
+                                            href={route("comprobantes.indexAdmin")}
+                                            activate={route().current("comprobantes.indexAdmin") ? "true" : "false"}
+                                        >
+                                            Comprobantes
+                                        </NavLink>
+                                    )}
                             </div>
                         </div>
 
