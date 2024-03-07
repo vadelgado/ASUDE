3f6b600ce7f1d9e559fddb2cc5c9bc9ef60b0ba1
vadelgado <vadelgado@udenar.edu.co>
2024-03-07 09:07:41 -0500

    07/03/2024 Algunos Registro jugadores inicial @vadelgado

---
+++ database/migrations/2024_03_06_220313_create_jugadores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jugadores', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipoDocIdentidad', 2)->default('1')->nullable(false);
+            $table->string('documentoIdentidad', 20)->nullable(false);
+            $table->string('nombreJugador', 50)->nullable(false);
+            $table->string('segundoNombreJugador', 50)->nullable(true);
+            $table->string('apellidoJugador', 50)->nullable(false);
+            $table->string('segundoApellidoJugador', 50)->nullable(true);
+            $table->date('fechaNacimiento')->nullable(false);
+            $table->unsignedBigInteger('fk_equipo')->nullable(false);
+            $table->foreign('fk_equipo')->references('id')->on('equipos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jugadores');
+    }
+};
