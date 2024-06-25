996d3b95610ed7817b3ef95d708775630001151d
vadelgado <vadelgado@udenar.edu.co>
2024-06-25 17:03:23 -0500

    ```text refactor: Update VerResultadosController.php to join resultados_partidos table
    This commit updates the VerResultadosController.php file to join the resultados_partidos table. The join condition includes multiple tables to retrieve data related to match results, including goals, yellow cards, and red cards. This change enhances the accuracy of the query results when displaying the results of a tournament.

--- app/Http/Controllers/VerResultadosController.php
+++ app/Http/Controllers/VerResultadosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\torneo;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class VerResultadosController extends Controller
@@ -20,7 +21,21 @@ class VerResultadosController extends Controller
         'torneo.imgBannerSuperior')
         ->get();
 
+        $resultados = DB::table('resultados_partidos as rs')
+        ->join('jugadores as j', 'rs.fk_jugador_id', '=', 'j.id')
+        ->join('equipos as e', 'j.fk_equipo', '=', 'e.id')
+        ->join('resultado_sorteos as rso', 'e.id', '=', 'rso.fk_equipo')
+        ->join('torneo as t', 'rso.fk_torneo', '=', 't.id')
+        ->where('t.id', $torneo_id)
+        ->select('e.nombreEquipo', DB::raw('SUM(rs.goles) as totalGoles'),
+        DB::raw('SUM(rs.tarjetas_amarillas) as tarjetasAmarillas'),
+        DB::raw('SUM(rs.tarjetas_rojas) as tarjetasRojas'))
+        ->groupBy('e.nombreEquipo')
+        ->get();
+        
+        dd($resultados, $torneo);
         return Inertia::render('VerResultados/Index',
-        [ 'torneo' => $torneo]);
+        [ 'torneo' => $torneo],
+        ['resultados' => $resultados]);
     }
 }
