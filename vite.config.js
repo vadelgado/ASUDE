58cd8dcd6370a2395acf722a94abedeec9e32b52
vadelgado <vadelgado@udenar.edu.co>
2024-07-25 06:55:11 -0500

    refactor: Update TrustProxies.php, package.json, and vite.config.js
    This commit updates TrustProxies.php, package.json, and vite.config.js.
    
    In TrustProxies.php:
    - Removed the assignment of '*' to the $proxies property, making it nullable.
    
    In package.json:
    - Removed the "watch" and "prod" scripts from the "scripts" section.
    
    In vite.config.js:
    - Removed the "https" configuration from the server options.
    - Removed the unused import statements.
    
    These changes improve the codebase by removing unused configurations and dependencies, ensuring a cleaner and more efficient project setup.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        https: true
-      },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
