c7a80dd795942a7bcd87358e6ec7494430cfac3c
vadelgado <vadelgado@udenar.edu.co>
2024-07-29 09:18:36 -0500

    refactor: Update routes and layout components

--- public/build/assets/ListOfTournaments-BpyWeHjK.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-DfQqMImO.js";import e from"./TournamentsItem-Cw2rgVK5.js";function o({torneo:r}){return t.jsxs("li",{children:[t.jsx("h3",{className:"mb-2 text-lg font-bold text-gray-800",children:r.nombreTorneo}),t.jsx(e,{torneo:r})]})}export{o as default};
